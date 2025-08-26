113da44f42d8cc52d2aead31f10dd90ac889846a
nestor-puerta <nestorpuerta2006@gmail.com>
2025-08-26 16:48:45 -0500

    actualizando

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/proyecto_three">
       <App />
     </BrowserRouter>
   </StrictMode>,
